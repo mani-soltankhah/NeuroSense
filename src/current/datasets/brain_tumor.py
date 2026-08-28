4a0a12a7c4bd7b3cfe5a6241bc4d55e1b16de0c9
Manisoltankhah <m.s.1384.mani@gmail.com>
2026-08-29 00:07:07 +0330

    second commit

--- src/current/datasets/brain_tumor.py
+++ src/current/datasets/brain_tumor.py
@@ -1,10 +1,16 @@
 import h5py
 from torch.utils.data import Dataset
+import torch
+import pandas as pd
+from pathlib import Path
+
+from src.current.utils.preprocessing import BrainMRIProcessor
 
 
 class BrainTumorDataset(Dataset):
-    def __init__(self, dataframe, image_transform=None, mask_transform=None):
+    def __init__(self, dataframe, processor=None, image_transform=None, mask_transform=None):
         self.df = dataframe.reset_index(drop=True)
+        self.processor = processor
         self.image_transform = image_transform
         self.mask_transform = mask_transform
 
@@ -18,4 +24,41 @@ class BrainTumorDataset(Dataset):
             cjdata = f['cjdata']
             image = cjdata['image'][()]
             tumor_mask = cjdata['tumorMask'][()]
+        if self.processor:
+            image, tumor_mask = self.processor.preprocess(image, tumor_mask)
         return image, tumor_mask
+
+
+data_dir = Path(r"D:\Portfolio\NeuroSense\Data\Raw")
+mat_files = list(data_dir.glob("*.mat"))
+df = pd.DataFrame(
+    {
+        "path": [
+            mat_files[0]
+        ],
+
+        "label": [
+            1,
+        ]
+    }
+)
+
+dataset = BrainTumorDataset(df)
+image, mask = dataset[0]
+
+processor = BrainMRIProcessor(
+    image_size=(224, 224)
+)
+
+dataset = BrainTumorDataset(
+    df,
+    processor=processor
+)
+
+image, mask = dataset[0]
+
+print(image.shape)
+print(mask.shape)
+print(image.dtype)
+print(mask.dtype)
+print(torch.unique(mask))
--- src/current/utils/preprocessing.py
+++ src/current/utils/preprocessing.py
@@ -1,97 +1,54 @@
 import random
 import torch
+import torch.nn.functional as F
 import h5py
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from src.current.datasets.brain_tumor import BrainTumorDataset
 
 
-def normalize_image(image):
-    image = image.astype('float32')
-    brain_mask = image != 0
-    brain_pixels = image[brain_mask]
-    mean = brain_pixels.mean()
-    std = brain_pixels.std()
-    normalized_image = np.zeros_like(image)
-    normalized_image[brain_mask] = (image[brain_mask] - mean) / std
-    return normalized_image
+class BrainMRIProcessor:
+    def __init__(self, image_size=(224, 224)):
+        self.image_size = image_size
 
+    def normalize_image(self, image):
+        image = image.astype('float32')
+        brain_mask = image != 0
+        brain_pixels = image[brain_mask]
+        mean = brain_pixels.mean()
+        std = brain_pixels.std()
+        normalized_image = np.zeros_like(image)
+        normalized_image[brain_mask] = (image[brain_mask] - mean) / std
+        return normalized_image
 
-def image_to_tensor(image):
-    tensor_image = torch.tensor(image)
-    return tensor_image
+    def image_to_tensor(self, image):
+        return torch.from_numpy(image)
 
+    def add_channel_dimension(self, image):
+        return image.unsqueeze(0)
 
-def add_channel_dimension(image):
-    return image.unsqueeze(0)
+    def mask_to_tensor(self, mask):
+        tensor_mask = torch.tensor(mask)
+        tensor_mask = tensor_mask.type(torch.float32)
+        return tensor_mask.unsqueeze(0)
 
+    def resize_image(self, image, size):
+        image = image.unsqueeze(0)
+        image = F.interpolate(image, size=size, mode='bilinear', align_corners=False)
+        return image.squeeze(0)
 
-def mask_to_tensor(mask):
-    tensor_mask = torch.tensor(mask)
-    tensor_mask = tensor_mask.type(torch.float32)
-    return tensor_mask.unsqueeze(0)
+    def resize_mask(self, image, size):
+        image = image.unsqueeze(0)
+        image = F.interpolate(image, size=size, mode='nearest')
+        return image.squeeze(0)
 
+    def preprocess(self, image, mask):
+        image = self.normalize_image(image)
+        image = self.image_to_tensor(image)
+        image = self.add_channel_dimension(image)
+        image = self.resize_image(image, self.image_size)
 
-data_dir = Path(r"D:\Portfolio\NeuroSense\Data\Raw")
-mat_files = list(data_dir.glob("*.mat"))
-df = pd.DataFrame(
-    {
-        "path": [
-            mat_files[0]
-        ],
+        mask = self.mask_to_tensor(mask)
+        mask = self.resize_mask(mask, self.image_size)
 
-        "label": [
-            1,
-        ]
-    }
-)
-
-dataset = BrainTumorDataset(df)
-image, mask = dataset[0]
-random_files = random.sample(mat_files, 10)
-normalized_image = normalize_image(image)
-normalized_tumor = normalized_image[mask == 1]
-normalized_background = normalized_image[mask == 0]
-tensor_image = image_to_tensor(normalized_image)
-tensor_image = add_channel_dimension(tensor_image)
-
-tensor_mask = mask_to_tensor(mask)
-
-print("Type:", type(tensor_mask))
-print("Dtype:", tensor_mask.dtype)
-print("Shape:", tensor_mask.shape)
-print("Unique:", torch.unique(tensor_mask))
-
-# for path in random_files:
-#     with h5py.File(path, "r") as f:
-#         cjdata = f["cjdata"]
-#         image = cjdata["image"][()]
-#         tumor_mask = cjdata['tumorMask'][()]
-#         normalized_random = normalize_image(image)
-#
-#         healthy_brain_mask = (image != 0) & (tumor_mask == 0)
-#         normalized_healthy_brain = normalized_random[healthy_brain_mask]
-#
-#         normalized_image = normalize_image(image)
-#         normalized_tumor = normalized_image[tumor_mask == 1]
-#         normalized_background = normalized_image[tumor_mask == 0]
-#         normalized_brain = normalized_random[normalized_random != 0]
-#         print(
-#             f"""
-#             Whole image:
-#             min: {normalized_random.min():.2f}
-#             max: {normalized_random.max():.2f}
-#             """
-#         )
-#         # print(f"""
-#         # name: {path.name}
-#         #
-#         # Tumor:
-#         # mean: {normalized_tumor.mean():.3f}
-#         # std: {normalized_tumor.std():.3f}
-#         #
-#         # Healthy brain:
-#         # mean: {normalized_healthy_brain.mean():.3f}
-#         # std: {normalized_healthy_brain.std():.3f}
-#         # """)
+        return image, mask
